e79623a52a20bff0a3899d4ce8c2a5ca5a2d3229
kubilay1994 <kubilay.guran@gmail.com>
2020-10-05 09:14:53 +0300

    tracker test edildi

---
+++ centroidTracker.py
@@ -0,0 +1,72 @@
+import numpy as np
+
+
+class CentroidTracker:
+    def __init__(self):
+        self.nextID = 0
+        self.objects = {}
+        self.dissappeared = {}
+        self.maxDissappeared = 30
+
+    def register(self, centroid):
+        self.objects[self.nextID] = centroid
+        self.dissappeared[self.nextID] = 0
+        self.nextID += 1
+
+    def remove(self, id):
+        del self.objects[id]
+        del self.dissappeared[id]
+
+    def update(self, rects):
+        if len(rects) == 0:
+            for id in list(self.dissappeared.keys()):
+                self.dissappeared[id] += 1
+
+                if self.dissappeared[id] > self.maxDissappeared:
+                    self.remove(id)
+            return self.objects
+
+        inputCentroids = np.asarray(
+            [(int((x + x + w) / 2), int((y + y + h) / 2)) for (x, y, w, h) in rects])
+
+        if len(self.objects) == 0:
+            for centroid in inputCentroids:
+                self.register(centroid)
+        else:
+            objectIDs = list(self.objects.keys())
+            centroids = np.array(list(self.objects.values()))
+
+            distances = np.linalg.norm(
+                centroids[:, np.newaxis] - inputCentroids, axis=-1)
+
+            rows = distances.min(axis=1).argsort()
+            cols = distances.argmin(axis=1)[rows]
+
+            usedRows = set()
+            usedCols = set()
+
+            for row, col in zip(rows, cols):
+                if row in usedRows or col in usedCols:
+                    continue
+
+                id = objectIDs[row]
+                self.objects[id] = inputCentroids[col]
+                self.dissappeared[id] = 0
+
+                usedRows.add(row)
+                usedCols.add(col)
+
+            unusedRows = set(range(len(centroids))) - usedRows
+            unusedCols = set(range(len(inputCentroids))) - usedCols
+
+            for row in unusedRows:
+                id = objectIDs[row]
+                self.dissappeared[id] += 1
+
+                if self.dissappeared[id] > self.maxDissappeared:
+                    self.remove(id)
+
+            for col in unusedCols:
+                self.register(inputCentroids[col])
+
+        return self.objects
--- playground.py
+++ playground.py
@@ -1,9 +1,7 @@
 import cv2
 import numpy as np
+
 import argparse
-
-from centroidTracker import CentroidTracker
-
 
 algorithms = dict(
     mog=cv2.bgsegm.createBackgroundSubtractorMOG,
@@ -18,28 +16,14 @@ parser = argparse.ArgumentParser(
 parser.add_argument("-a", "--algo", help="Arkaplan algoritması", type=str,
                     choices=algorithms.keys(), default="mog2")
 
-parser.add_argument("-o", "--out", help="Video kaydı", type=str, default=None)
-
+# parser.add_argument("-v", "--video", type=str, help="Video dosyasının yolu")
 args = parser.parse_args()
 
-
 cap = cv2.VideoCapture(0)
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
 
-out = cv2.VideoWriter(f'videos/{args.out}.avi', fourcc, 20.0,
-                      (int(cap.get(3)), int(cap.get(4))))
-
-ct = CentroidTracker()
+frame_num = 0
 backSub = algorithms[args.algo]()
 
-
-for i in range(100):
-    ret, frame = cap.read()
-    if ret == False:
-        break
-    backSub.apply(frame)
-
-max_id = 0
 while True:
 
     ret, frame = cap.read()
@@ -52,49 +36,30 @@ while True:
 
     if args.algo == "gmg":
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-        # mask = cv2.medianBlur(mask, 5)
         mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
         mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN,  kernel)
-        mask = cv2.erode(mask, kernel)
+        mask = cv2.dilate(mask, kernel)
+        # _, mask = cv2.threshold(mask, 127, 255, cv2.THRESH_BINARY)
 
     contours, _ = cv2.findContours(
         mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+    contours = [
+        contour for contour in contours if cv2.contourArea(contour) > 1000]
+
     for contour in contours:
         (x, y, w, h) = cv2.boundingRect(contour)
-
-        if cv2.contourArea(contour) > 1200 and w > 40 and h > 40:
-            rects.append((x, y, w, h))
-            rects.append((x, y, w, h))
-
-    rects, _ = cv2.groupRectangles(rects, 1, 0.5)
-
-    objects = ct.update(rects)
-
-    for x, y, w, h in rects:
+        rects.append((x, y, w, h))
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 0), 3)
 
-    for id, (cx, cy) in objects.items():
-
-        max_id = max(max_id, id)
-        text = f"ID {id}"
-        cv2.putText(frame, text, (cx - 10, cy - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (80, 80, 200), 2)
-        cv2.circle(frame, (cx, cy), 4, (80, 255, 200), -1)
-
-    cv2.putText(frame, f"Toplam nesne sayisi : {max_id + 1}", (0, 50),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5,  (255, 0, 0), 3)
+    frame_num += 1
 
     cv2.imshow("normal", frame)
     cv2.imshow("mask", mask)
-
-    if args.out:
-        out.write(frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 
 cap.release()
-out.release()
 cv2.destroyAllWindows()
